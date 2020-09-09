39b281f4e15d41e4bf5e01ba02740b88e47ba76a
Tadashi Ringo <phamt8611@gmail.com>
2020-09-09 15:28:31 +0700

    Đánh dấu dòng command vào phần add và commit trong code để chúng tạm thời ngừng hoạt động lại - Thêm phần lựa chọn chi nhánh git branch - Thêm chuyển đổi qua lại giữa các chi nhánh

--- Git-CMT/Git-Cmt.py
+++ Git-CMT/Git-Cmt.py
@@ -1,11 +1,21 @@
 import os
 
 ringo_login = "login.py"
-
+print("1. Chi Nhánh")
+player = input("Lựa chọn: ")
+if player == "1":
+    print("1.Chọn chi nhánh\n2.Tạo chi nhánh\n3.Xóa chi nhánh(Chưa tạo)")
+    player = input("Nhập(Mặc định =1):")
+    if player == "1":
+        os.system("git branch")
+        player = input("Nhập tên chi nhánh: ")
+        os.system("git checkout %s"%player)
+'''
 print("1. ringo-login.py")
 player = input("Nhập: ")
 
 if player == "1":
     player_commit = input("Nhập vào Commit của %s: "%ringo_login)
     os.system("git add %s && git commit -m \"%s\""%( ringo_login , player_commit))
-        
+
+'''
