30651134970d5e568a1f2c4a7ac9ddb751488ee8
Tadashi Ringo <phamt8611@gmail.com>
2020-09-09 13:40:55 +0700

    login.py đã được cập nhật,công năng vẫn như thế,chưa có sự thay đổi, chương trình tồn tại thiếu sót đó là không cần nào logout

---
+++ login.py
@@ -0,0 +1,21 @@
+import os
+print("/////\\\\\\\\")
+print("||||||||||")
+print("\\\\\\\\/////")
+print(" ")
+admin_user = "Tadashi Ringo"
+admin_pass = "tadashiringo123"
+print("-------")
+print("Nhập vào tài khoản của bạn!!!")
+print("-------")
+player_user = input("Tên: ")
+player_pass = input("Mật Khẩu:  ")
+while True:
+    if player_user == admin_user and player_pass == admin_pass:
+        os.system("echo Đăng nhập thành công")
+        break
+    else:
+        os.system("echo Đăng nhập thất bại")
+        os.system("echo tên hoặc mật khẩu không đúng,vui lòng nhập lại")
+        player_user = input("Nhập tên  ngừi dùng: ")
+        player_pass = input("Nhập mật khẩu người dùng: ")
