177f1715ae656b8b0c31df588c6f38c9bba235ae
Tadashi Ringo <phamt8611@gmail.com>
2020-09-10 14:16:51 +0700

    n

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-home_dir = os.system("cd ~")
-print("`cd ~` ran with exit code %d" % home_dir)
-
-unknown_dir = os.system("cd doesnotexist")
-print("`cd doesnotexis` ran with exit code %d" % unknown_dir)
